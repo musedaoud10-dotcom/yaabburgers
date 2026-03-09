4abdde286c6039352987e9feb9809eb148486af0
musedaoud10-dotcom <muse.daoud10@gmail.com>
2026-03-09 01:16:45 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 const tabs = document.querySelectorAll(".tab")
 const categories = document.querySelectorAll(".menu-category")
 
-tabs.forEach(tab=>{
+tabs.forEach(tab => {
 
-tab.addEventListener("click",()=>{
+tab.addEventListener("click", () => {
 
-tabs.forEach(t=>t.classList.remove("active"))
-categories.forEach(c=>c.classList.remove("active"))
+tabs.forEach(t => t.classList.remove("active"))
+categories.forEach(c => c.classList.remove("active"))
 
 tab.classList.add("active")
 
